8f3b15dfdbae50b70cb47c7cc7007285bcffd49b
tacowasa059 <wasserstein59@gmail.com>
2025-08-15 19:11:55 +0900

    [modifiy] mohistでの動作を確認

--- src/main/java/com/github/tacowasa059/displaydistancemod/mixin/DisplayMixin.java
+++ src/main/java/com/github/tacowasa059/displaydistancemod/mixin/DisplayMixin.java
@@ -2,6 +2,8 @@ package com.github.tacowasa059.displaydistancemod.mixin;
 
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.world.entity.Display;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,5 +22,10 @@ public class DisplayMixin {
     private void setViewRange(float p_270907_, CallbackInfo ci) {
         ((Display)(Object)this).getEntityData().set(DATA_VIEW_RANGE_ID, 512f * 8);
         ci.cancel();
+    }
+
+    @Inject(method = "<init>", at=@At("TAIL"))
+    private void onInit(EntityType<Display> p_270360_, Level p_270280_, CallbackInfo ci){
+        ((Display)(Object)this).getEntityData().set(DATA_VIEW_RANGE_ID, 512f * 8);
     }
 }
--- src/main/java/com/github/tacowasa059/displaydistancemod/mixin/TrackedEntityMixin.java
+++ src/main/java/com/github/tacowasa059/displaydistancemod/mixin/TrackedEntityMixin.java
@@ -1,5 +1,6 @@
 package com.github.tacowasa059.displaydistancemod.mixin;
 
+import com.github.tacowasa059.displaydistancemod.config.DDConfig;
 import net.minecraft.server.level.ServerEntity;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerPlayerConnection;
@@ -25,8 +26,6 @@ public abstract class TrackedEntityMixin {
     @Final
     @Shadow
     private Set<ServerPlayerConnection> seenBy;
-    @Shadow
-    protected abstract int getEffectiveRange();
 
     // サーバーの描画距離設定を無視
     @Inject(method = "updatePlayer", at = @At("HEAD"), cancellable = true)
@@ -34,10 +33,12 @@ public abstract class TrackedEntityMixin {
         if(this.entity instanceof Display){
             if (p_140498_ != this.entity) {
                 Vec3 vec3 = p_140498_.position().subtract(this.entity.position());
-                double d0 = this.getEffectiveRange();
+                double d0 = 16 * (DDConfig.displayRangeChunks());
                 double d1 = vec3.x * vec3.x + vec3.z * vec3.z;
                 double d2 = d0 * d0;
+
                 boolean flag = d1 <= d2 && this.entity.broadcastToPlayer(p_140498_);
+
                 if (flag) {
                     if (this.seenBy.add(p_140498_.connection)) {
                         this.serverEntity.addPairing(p_140498_);
